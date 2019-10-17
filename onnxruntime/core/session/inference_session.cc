76c1f590ff9a0409939432b2a5431c59f2a74e25
Changming Sun <chasun@microsoft.com>
2019-10-17 09:47:00 -0700

    ETW changes

--- onnxruntime/core/session/inference_session.cc
+++ onnxruntime/core/session/inference_session.cc
@@ -627,6 +627,28 @@ common::Status InferenceSession::Initialize() {
     // handle any subgraphs
     ORT_RETURN_IF_ERROR_SESSIONID_(InitializeSubgraphSessions(graph, session_state_));
     is_inited_ = true;
+#ifdef ONNXRUNTIME_ENABLE_INSTRUMENT
+    std::map<std::string, size_t> node_name_to_index;
+    bool AreNamesUnique = true;
+    for (auto& n : session_state_.GetGraphViewer()->Nodes()) {
+      if (n.Name().empty() || node_name_to_index.find(n.Name()) != node_name_to_index.end()) {
+        AreNamesUnique = false;
+        break;
+      }
+    }
+    std::ostringstream oss;
+    if (AreNamesUnique) {
+      for (auto& kvp : node_name_to_index) {
+        oss << kvp.first << " " << kvp.second << "\n";
+      }
+      std::string content = oss.str();
+      // TODO: write the information out
+      TraceLoggingWrite(telemetry_provider_handle,  // handle to my provider
+                        "NodeNameMapping",  // Event Name that should uniquely identify your event.
+                        TraceLoggingValue(content.c_str(), "op_name"));
+    }
+
+#endif
     LOGS(*session_logger_, INFO) << "Session successfully initialized.";
   } catch (const NotImplementedException& ex) {
     status = ORT_MAKE_STATUS(ONNXRUNTIME, NOT_IMPLEMENTED, "Exception during initialization: ", ex.what());
