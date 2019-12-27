f7cc68e306fb5886295dd86286466f56d54dea0a
Changming Sun <chasun@microsoft.com>
2019-12-26 16:02:38 -0800

    update

--- tools/ci_build/github/windows/run_OpenCppCoverage.ps1
+++ tools/ci_build/github/windows/run_OpenCppCoverage.ps1
@@ -32,6 +32,26 @@ function RunTest([string]$test_cmd, [string[]]$test_cmd_args, [string[]]$export_
     & "C:\Program Files\OpenCppCoverage\OpenCppCoverage.exe" $cmdParams
 }
 
+function RunTest2([string]$test_cmd, [string[]]$export_types, [string[]]$inputs)
+{
+    $cmdParams = @("--sources=$headerSources","--sources=$coreSources","--modules=$buildDir","--working_dir=$buildDir")
+
+    foreach($input in $inputs)
+    {
+        $inputPath = Join-Path $buildDir $input
+        $cmdParams += "--input_coverage=$inputPath"
+    }
+
+    foreach($export_type in $export_types)
+    {
+        $cmdParams += "--export_type=$export_type"
+    }
+
+    $cmdParams += @("--","$test_cmd")
+    Write-Host "$cmd $cmdParams"
+    & "C:\Program Files\OpenCppCoverage\OpenCppCoverage.exe" $cmdParams
+}
+
 # generate cobertura xml output and html report
 $outputXml = Join-Path $buildDir "cobertura.xml"
 $outputDir = Join-Path $buildDir "OpenCppCoverageResults"
@@ -43,14 +63,14 @@ $onnx_test_runner = Join-Path $buildDir "onnx_test_runner.exe"
 RunTest $onnx_test_runner ($modelDir) ("binary:"  + (Join-Path $buildDir "onnx_test_runner.cov"))
 
 
-#Write-Host "C-API/Shared-lib test"
-#$shared_lib_test = Join-Path $buildDir "onnxruntime_shared_lib_test.exe"
-#RunTest $shared_lib_test @() ("binary:" + (Join-Path $buildDir "onnxruntime_shared_lib_test.cov"))
+Write-Host "C-API/Shared-lib test"
+$shared_lib_test = Join-Path $buildDir "onnxruntime_shared_lib_test.exe"
+RunTest2 $shared_lib_test ("binary:" + (Join-Path $buildDir "onnxruntime_shared_lib_test.cov"))
 
 
 Write-Host "MLAS test"
 $mlas_test = Join-Path $buildDir "onnxruntime_mlas_test.exe"
-RunTest $mlas_test @() ("binary:" + (Join-Path $buildDir "onnxruntime_mlas_test.cov"))
+RunTest2 $mlas_test ("binary:" + (Join-Path $buildDir "onnxruntime_mlas_test.cov"))
 
 Write-Host "Lotus unit tests"
 # need to copy the tvm.dll, since it is not in the buildDir path
@@ -59,5 +79,5 @@ if (Test-Path -Path $BuildRoot\Debug\external\tvm\Debug\tvm.dll -PathType Leaf) 
 }
 
 $onnxruntime_test_all = Join-Path $buildDir "onnxruntime_test_all.exe"
-RunTest $onnxruntime_test_all @() ("cobertura:$outputXml","html:$outputDir") ("onnxruntime_shared_lib_test.cov","onnx_test_runner.cov","onnxruntime_mlas_test.cov")
+RunTest2 $onnxruntime_test_all ("cobertura:$outputXml","html:$outputDir") ("onnxruntime_shared_lib_test.cov","onnx_test_runner.cov","onnxruntime_mlas_test.cov")
 
